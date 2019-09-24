44766408cc898c570d34c018ff3ccc69e9772195
Max Reinmueller <maxreinmueller@gmail.com>
2019-09-24 12:58:02 -0700

    application populating web page

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(express.json());
 
 var PORT = process.env.PORT || 8080;
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+
 app.listen(PORT, function() {
     console.log('app running on http//localhost:' + PORT);
 });
