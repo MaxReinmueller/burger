0d15e0cf22ce3de06cc1b1f9841c177c026b1d7b
Max Reinmueller <maxreinmueller@gmail.com>
2019-09-24 08:32:50 -0700

    set up mysql connection

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Flying saucer-er', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Avacado', false);
